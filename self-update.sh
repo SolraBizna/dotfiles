aed77dd7b87d7cd4387d812b45e5f5a20ce5f5c7
Solra Bizna <solra@bizna.name>
2021-08-27 12:07:33 -0600

    Improve the logic a bit

--- self-update.sh
+++ self-update.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+# exit self-update early if somebody else recently did it
+# (stops the "all six terminals updating" problem)
+if [ ! \( -z "$__DID_SELF_UPDATE" -a -z "$__NO_SELF_UPDATE" -a \
+       ! -f ~/.dotfiles-no-self-update \) ]; then exit; fi
+
 cd ~/.dotfiles
 echo "Checking for dotfile updates..."
-git pull
+git pull --ff-only
 touch .last-self-update
