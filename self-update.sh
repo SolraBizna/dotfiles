b2d642a3e567b80fe9b577326eb6c58101f3a293
Solra Bizna <solra@bizna.name>
2023-05-08 08:02:50 -0600

    Finish what I started, fix the parallel update problem

--- self-update.sh
+++ self-update.sh
@@ -2,10 +2,13 @@
 
 set -e
 
-# exit self-update early if somebody else recently did it
-# (stops the "all six terminals updating" problem)
 if [ ! \( -z "$__DID_SELF_UPDATE" -a -z "$__NO_SELF_UPDATE" -a \
        ! -f ~/.dotfiles-no-self-update \) ]; then exit; fi
+# exit self-update early if somebody else recently did it
+# (stops the "all six terminals updating" problem)
+if [ ! -z "$(find ~/.dotfiles/ -name .last-self-update -a -mtime -7)" ]; then
+    exit
+fi
 
 cd ~/.dotfiles
 echo "Checking for dotfile updates..."
